57dae33d45984b97a9c54d885571dd6313e07bed
David Dieudonné <122728110+Mochi-Yuki@users.noreply.github.com>
2023-11-22 12:53:21 +0100

    Add files via upload

--- Version I (no 3d flip-card-animation in form)/js/utils.js
+++ Version I (no 3d flip-card-animation in form)/js/utils.js
@@ -1,53 +1,48 @@
-// clicking link 'Already Member?' switches from 'Sign Up' section to 'Sign In' section
+const frontIcon = document.getElementById('toggle-icon-front');
+const backIcon = document.getElementById('toggle-icon-back');
 
-const switchToSignIn = document.getElementById('switchToSignIn');
+//get the associated password input element for sign-in and sign-up
+const passwordSignIn = document.querySelector('.sign-in-card input[name="password"]');
+const passwordSignUp = document.querySelector('.sign-up-card input[name="password"]');
 
-switchToSignIn.addEventListener('click', function() {
-    document.getElementById('tab-1').click();
-    resetIconVisibility();
+function resetIconToHide() {
+    frontIcon.classList.remove('bxs-show');
+    frontIcon.classList.add('bxs-hide');
+
+    backIcon.classList.remove('bxs-show');
+    backIcon.classList.add('bxs-hide');
+
+    passwordSignIn.type = 'password';
+    passwordSignUp.type = 'password';
+}
+
+function toggleIconVisibility(icon) {
+    icon.classList.toggle('bxs-hide');
+    icon.classList.toggle('bxs-show');
+
+    //set password field type based on icon display state
+    passwordSignIn.type = icon.classList.contains('bxs-show') ? 'text' : 'password';
+    passwordSignUp.type = icon.classList.contains('bxs-show') ? 'text' : 'password';
+}
+
+frontIcon.addEventListener('click', function() {
+    toggleIconVisibility(frontIcon);
 });
 
+backIcon.addEventListener('click', function() {
+    toggleIconVisibility(backIcon);
+});
 
-// clicking password icons switches their visibility modus & input type
-
-function togglePasswordVisibility(passwordField, hideIcon, showIcon) {
-    passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
-    hideIcon.style.display = passwordField.type === 'password' ? 'inline' : 'none';
-    showIcon.style.display = passwordField.type === 'password' ? 'none' : 'inline';
-}
-
-function addToggleListener(passwordField, hideIcon, showIcon) {
-    hideIcon.addEventListener('click', () => togglePasswordVisibility(passwordField, hideIcon, showIcon));
-    showIcon.addEventListener('click', () => togglePasswordVisibility(passwordField, showIcon, hideIcon));
-}
-
-// apply to elements in Sign In card
-const passwordSignIn = document.querySelector('.sign-in-card input[name="password"]');
-const hideFront = document.getElementById('hide-front');
-const showFront = document.getElementById('show-front');
-addToggleListener(passwordSignIn, hideFront, showFront);
-
-// apply to elements in Sign Up card
-const passwordSignUp = document.querySelector('.sign-up-card input[name="password"]');
-const hideBack = document.getElementById('hide-back');
-const showBack = document.getElementById('show-back');
-addToggleListener(passwordSignUp, hideBack, showBack);
-
-
-// function to reset password icon visibility & input type to default state
-
-function resetIconVisibility() {
-    hideFront.style.display = 'inline';
-    showFront.style.display = 'none';
-    hideBack.style.display = 'inline';
-    showBack.style.display = 'none';
-    
-    passwordSignIn?.setAttribute('type', 'password');
-    passwordSignUp?.setAttribute('type', 'password');
-}
-
+//clicking either radio button resets default state of visibility icon
 const signInRadio = document.getElementById('tab-1');
 const signUpRadio = document.getElementById('tab-2');
 
-signInRadio.addEventListener('click', resetIconVisibility);
-signUpRadio.addEventListener('click', resetIconVisibility);
+signInRadio.addEventListener('click', resetIconToHide);
+signUpRadio.addEventListener('click', resetIconToHide);
+
+// clicking link 'Already Member?' switches from 'Sign Up' section to 'Sign In' section 
+const switchToSignIn = document.getElementById('switchToSignIn');
+switchToSignIn.addEventListener('click', function() {
+    document.getElementById('tab-1').click();
+    resetIconToHide();
+});
